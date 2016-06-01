ce3bb070491ea97d606b5b4b53c50891d79dd8f7
Al Johri <al.johri@gmail.com>
2016-06-01 06:32:42 -0400

    create download.sh

---
+++ download.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+youtube-dl "https://www.youtube.com/watch?v=-7I7MWTX0gA" # [Jake Vanderplas - Statistics for Hackers]
+youtube-dl "https://www.youtube.com/watch?v=6GV5bTCLC8g" # [Allen Downey - Bayesian statistics made simple]
+youtube-dl "https://www.youtube.com/watch?v=YgtL4S7Hrwo" # [Guido van Rossum - Python Language]
+youtube-dl "https://www.youtube.com/watch?v=znfy3T9OiAQ" # [Kevin Markham - Machine Learning with Text in scikit-learn]
+youtube-dl "https://www.youtube.com/watch?v=VR52vSbHBAk" # [Allen Downey - Computational Statistics]
+youtube-dl "https://www.youtube.com/watch?v=ll6Tq-wTXXw" # [Karen Rubin - Building a Quantitative Trading Strategy To Beat the S&P500](https:/
+youtube-dl "https://www.youtube.com/watch?v=n4VLLQXF_9Y" # [Sev Leonard - The Fellowship of the Data]
+youtube-dl "https://www.youtube.com/watch?v=nb3GRgtjlTw" # [Tyler Reddy - Computational Geometry in Python]
+youtube-dl "https://www.youtube.com/watch?v=uW02_GnQKeM" # [Wendy Grus - When is it good to be bad? Web scraping and data analysis of NHL penalties]
+youtube-dl "https://www.youtube.com/watch?v=5gy6svL1DuU" # [IPython Notebook in Data Intensive Communities: Accelerating the process of Discovery]
+youtube-dl "https://www.youtube.com/watch?v=kVud83kqv30" # [Irene Chen - A Beginner's Guide to Deep Learning]
+youtube-dl "https://www.youtube.com/watch?v=SJvhQroy7hY" # [Elizabeth Ramirez - Kalman Filters for non-rocket science]
+youtube-dl "https://www.youtube.com/watch?v=Ws34Ho-1aDs" # [Tracy Teal - Data Carpentry: An Introduction to Python for Data Analysis and ](https:/
+youtube-dl "https://www.youtube.com/watch?v=oSSnDeOXTZQ" # [Making an Impact with Python Natural Language Processing Tools]
